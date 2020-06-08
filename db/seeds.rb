b6d151652ce51dada55e0137334799c54382f215
Derek Borski <derek.borski@gmail.com>
2020-06-08 09:59:15 -0600

    Update seed to have doctors be part of a hospital

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# #doctors
-# doctor1 = Doctor.create(name: "Meredith Grey", speciality: "General Surgery", education: "Harvard University")
-# doctor2 = Doctor.create(name: "Alex Karev", speciality: "Pediatric Surgery", education: "Johns Hopkins University")
-# doctor3 = Doctor.create(name: "Miranda Bailey", speciality: "General Surgery", education: "Stanford University")
-# doctor4 = Doctor.create(name: "Derek McDreamy Shepherd", speciality: "Attending Surgeon", education: "University of Pennsylvania")
+Doctor.destroy_all
+Hospital.destroy_all
+Patient.destroy_all
 
-# #hospitals
-# hospital1 = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
-# hospital2 = Hospital.create(name: "Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001")
 
-# #patients
-# patient1 = Patient.create(name: "Katie Bryce", age: 24)
-# patient2 = Patient.create(name: "Denny Duquette", age: 39)
-# patient3 = Patient.create(name: "Rebecca Pope", age: 32)
-# patient4 = Patient.create(name: "Zola  Shepherd", age: 2)
+
+#hospitals
+hospital1 = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+hospital2 = Hospital.create(name: "Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001")
+
+#doctors
+doctor1 = hospital1.doctors.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")
+doctor2 = hospital1.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
+doctor3 = hospital2.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
+doctor4 = hospital2.doctors.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania")
+
+#patients
+patient1 = Patient.create(name: "Katie Bryce", age: 24)
+patient2 = Patient.create(name: "Denny Duquette", age: 39)
+patient3 = Patient.create(name: "Rebecca Pope", age: 32)
+patient4 = Patient.create(name: "Zola  Shepherd", age: 2)
