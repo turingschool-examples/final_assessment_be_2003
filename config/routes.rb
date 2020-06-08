dcde72c2565db44df7c6e16ce3a75411d7998924
Antonio Jackson <antoniojackson@gmail.com>
2020-06-08 14:45:14 -0600

    Replacing hospitals show route with hospitals show resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :doctors, only: [:show, :edit, :update]
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#destroy'
 
-  get '/hospitals/:id', to: 'hospitals#show'
+  resources :hospitals, only: [:show]
   get '/hospitals/:hospital_id/patients', to: 'hospital_patients#index'
 
   get '/hospitals/:hospital_id/doctors/new', to: 'doctors#new'
