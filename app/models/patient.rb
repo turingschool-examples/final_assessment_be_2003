776f498fb74e41bc8f239236e13418166b23e355
Mariana Cid <cidmariana5@gmail.com>
2020-06-08 14:27:21 -0600

    new method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,8 @@ class Patient < ApplicationRecord
   validates_presence_of :name, :age
   has_many :patient_doctors
   has_many :doctors, through: :patient_doctors
+
+  def ordered_names
+    Self.order(:age)
+  end
 end
