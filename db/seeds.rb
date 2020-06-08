053509c22e5a6e27e9717f6d4829998f5f8b9842
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-08 09:43:17 -0600

    Create doctors and hospitals associations) '

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Doctors:
+Doctor.create(
+Name: 'Meredith Grey',
+Specialty: 'General Surgery',
+Education: 'Harvard University')
+
+Doctor.create(
+Name: 'Alex Karev',
+Specialty: 'Pediatric Surgery',
+Education: 'Johns Hopkins University')
+
+Doctor.create(
+Name: 'Miranda Bailey',
+Specialty: 'General Surgery',
+Education: 'Stanford University')
+
+Doctor.create(
+Name: 'Derek McDreamy Shepherd',
+Specialty: 'Attending Surgeon',
+Education: 'University of Pennsylvania')
+
+
+# Hospitals:
+
+Hospital.create(
+name: 'Grey Sloan Memorial Hospital',
+address: '123 Save Lives Rd',
+city: 'Seattle',
+state: 'WA',
+zip: 98101)
+
+Hospital.create(
+name: 'Seaside Health & Wellness Center',
+address: '123 Private Practice Road',
+city: 'Los Angeles',
+state: 'CA,'
+zip: 90001)
+
+# Patients:
+Patient.create(
+Name: 'Katie Bryce',
+Age: 24)
+
+Patient.create(
+Name: 'Denny Duquette',
+Age: 39)
+
+Patient.create(
+Name: 'Rebecca Pope',
+Age: 32)
+
+Patient.create(
+Name: 'Zola  Shepherd',
+Age: 2)
