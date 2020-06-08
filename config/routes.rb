09917352c259b9cb75f4df97d6ad1c6389ee508f
Antonio Jackson <antoniojackson@gmail.com>
2020-06-08 14:47:44 -0600

    Replacing hospital doctors new route with hospital doctors new resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :doctors, only: [:show, :edit, :update]
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#destroy'
 
-  resources :hospitals, only: [:show]
+  resources :hospitals, only: [:show] do
+    resources :doctors, only: [:new]
+  end
   get '/hospitals/:hospital_id/patients', to: 'hospital_patients#index'
 
-  get '/hospitals/:hospital_id/doctors/new', to: 'doctors#new'
   post '/hospitals/:hospital_id/doctors', to: 'doctors#create'
 end
