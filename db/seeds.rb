3f12c71617c1fc11a66266b49ce2738418d8b101
Jack Gould <johnhilsmangould@gmail.com>
2020-06-08 09:34:31 -0600

    add test for story 1 - add read me seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Doctors:
+  # Name: Meredith Grey
+  # Specialty: General Surgery
+  # Education: Harvard University
+  # Name: Alex Karev
+  # Specialty: Pediatric Surgery
+  # Education: Johns Hopkins University
+  # Name: Miranda Bailey
+  # Specialty: General Surgery
+  # Education: Stanford University
+  # Name: Derek McDreamy Shepherd
+  # Specialty: Attending Surgeon
+  # Education: University of Pennsylvania
+# Hospitals:
+  # Grey Sloan Memorial Hospital
+  # 123 Save Lives Rd
+  # Seattle, WA 98101
+  # Seaside Health & Wellness Center
+  # 123 Private Practice Road
+  # Los Angeles, CA 90001
+# Patients:
+  # Name: Katie Bryce
+  # Age: 24
+  # Name: Denny Duquette
+  # Age: 39
+  # Name: Rebecca Pope
+  # Age: 32
+  # Name: Zola  Shepherd
+  # Age: 2
---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe "Doctors show page", type: :feature do
+  before :each do
+    @hospital_1 = Hospital.create!(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101" )
+    @hospital_2 = Hospital.create!(name: "Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001")
+    @doc_1 = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: @hospital_1.id)
+    @doc_2 = Doctor.create!(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: @hospital_1.id)
+    @doc_3 = Doctor.create!(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", hospital_id: @hospital_2.id)
+    @doc_4 = Doctor.create!(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania", hospital_id: @hospital_2.id)
+    @patient_1 = Patient.create!(name: "Katie Bryce", age: 24)
+    @patient_2 = Patient.create!(name: "Denny Duquette", age: 39)
+    @patient_3 = Patient.create!(name: "Rebecca Pope", age: 32)
+    @patient_4 = Patient.create!(name: "Zola Shepherd", age: 2)
+
+    PatientDoctors.create!(doctor_id: @doc_1.id, patient_id: @patient_1.id)
+    PatientDoctors.create!(doctor_id: @doc_1.id, patient_id: @patient_2.id)
+    PatientDoctors.create!(doctor_id: @doc_1.id, patient_id: @patient_3.id)
+    PatientDoctors.create!(doctor_id: @doc_2.id, patient_id: @patient_1.id)
+    PatientDoctors.create!(doctor_id: @doc_2.id, patient_id: @patient_3.id)
+  end
+
+  it "Story-1: Doctors info, hospital and patients are displayed" do
+    visit "/doctors/#{@doc_1.id}"
+
+    expect(page).to have_content("#{@doc_1.name}")
+    expect(page).to have_content("#{@doc_1.specialty}")
+    expect(page).to have_content("#{@doc_1.education}")
+    expect(page).to have_content("#{@hospital_1.name}")
+    expect(page).to have_content("#{@patient_1.name}")
+    expect(page).to have_content("#{@patient_2.name}")
+    expect(page).to have_content("#{@patient_3.name}")
+  end
+
+end
