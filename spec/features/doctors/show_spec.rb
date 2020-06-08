03275465b71d40cdd1517bbc1e05e2e91833a1f4
Mad Hallé <mad.halle@icloud.com>
2020-06-08 12:34:01 -0600

    user story 6 testing begun

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe "when visiting a doctors show page" do
     state: "WA",
     zip: 98101)
 
+    @seaside = Hospital.create!(name: "Seaside Health & Wellness Center",
+    address: "123 Private Practice Road",
+    city: "Los Angeles",
+    state: "CA",
+    zip: 90001)
+
 
     @meredith = @greys.doctors.create!(name: "Meredith Grey",
                               specialty: "General Surgery",
@@ -42,4 +48,29 @@ RSpec.describe "when visiting a doctors show page" do
     expect(page).to have_content(@denny.name)
   end
 
+  it "a visitor will see all doctors info, hospital & patients" do
+    visit "/doctors/#{@meredith.id}"
+
+    click_link "Assign #{@meredith.name} to a Different Hospital"
+    expect(current_path).to eq("/doctors/#{@meredith.id}/edit")
+
+    fill_in :hospital_id, with: @seaside.id
+
+    click_on "Add #{@meredith.name} to this hospital"
+    expect(current_path).to eq("/doctors/#{@meredith.id}")
+
+    expect(page).to have_content(@seaside.name)
+  end
 end
+# ```
+# User Story 6, Reassign Doctor to Different Hospital
+# As a visitor
+# When I visit a doctor's show page
+# Next to the name of the hospital where this doctor works
+# I see a link that says "Assign <insert name of doctor> to a Different Hospital"
+# When I click on that link
+# I'm taken to a form where I can input an id of an existing hospital
+# When I click "Add <insert name of doctor> to this hospital"
+# I'm taken back to that doctor's show page
+# And I can see the name of the new hospital that they were assigned to
+# ```
