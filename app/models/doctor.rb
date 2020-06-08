43423b387f996544b9662510c7f642ee5fbd611c
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-08 12:29:19 -0600

    add doctor class

---
+++ app/models/doctor.rb
@@ -0,0 +1,10 @@
+class Doctor < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :specialty
+  validates_presence_of :education
+
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
+
+  belongs_to :hospital
+end
