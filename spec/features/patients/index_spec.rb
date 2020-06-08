970b4ae3e8ab8bbedc94b2b15b43b4a42b20f58c
Michael Gallup <Gallup93@icloud.com>
2020-06-08 11:37:28 -0600

    Add patients index spec

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Patient index page" do
+  before(:each) do
+    @greys = Hospital.create(name:"Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "Washington", zip: "98101")
+
+    @meredith = @greys.doctors.create(name: "Meredith Grey", specialty: "general surgery", school: "Turing")
+    @joe = @greys.doctors.create(name: "Joe", specialty: "rashes", school: "Brown")
+
+    @mikal = @meredith.patients.create(name: "Mikal", age: 27)
+    @austin = @meredith.patients.create(name: "Austin", age: 26)
+    @jared = @joe.patients.create(name: "Jared", age: 28)
+  end
+
+  it "lists all patients from oldest to youngest" do
+    visit "/hospitals/#{@greys.id}/patients"
+
+    expect(page).to have_content("Jared Mikal Austin")
+  end
+
+end
