2b6eb0739292662d44fdf87c0363c7abfbac9d1a
Michael Gallup <Gallup93@icloud.com>
2020-06-08 09:35:39 -0600

    Add doctors to db, fix typo

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
-  belongs_to :hospitals
+  belongs_to :hospital
   has_many :doc_patients
   has_many :patients, through: :doc_patients
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608152205) do
+ActiveRecord::Schema.define(version: 20200608153304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.string "school"
+    t.bigint "hospital_id"
+  end
 
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
