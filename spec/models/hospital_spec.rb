1709e9668de1c4ab3acd22d4c96231ad89927c1f
Jack Cullen <jpcullen20@gmail.com>
2020-06-08 09:33:09 -0600

    Create hospital model

---
+++ spec/models/hospital_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Hospital do
+
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip}
+  end
+
+  describe 'Relationships' do
+    it {should have_many :doctors}
+  end
+
+end
