7544bec6ad2071c2273b00495d8497e862b8042f
Mariana Cid <cidmariana5@gmail.com>
2020-06-08 12:55:17 -0600

    hospital uniq education method

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,11 @@
 class Hospital < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :doctors
+
+
+  def uniq_education
+    doctors.select('education')
+           .distinct
+           .pluck('education')
+  end
 end
