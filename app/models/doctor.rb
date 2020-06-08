1f62aef6c2b8516fb1f3f8401bad830b7e89309c
Antonio Jackson <antoniojackson@gmail.com>
2020-06-08 09:48:03 -0600

    Finishing creating all tables and establishing all relationships

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
   validates_presence_of :name, :specialty, :education
   belongs_to :hospital
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
 end
