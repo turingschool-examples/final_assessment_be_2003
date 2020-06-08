098d2d3064ff6d2352f38dbbc223ce564fef1ce5
Derek Borski <derek.borski@gmail.com>
2020-06-08 11:00:02 -0600

    full_address instance method

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,4 +1,7 @@
 class Hospital < ApplicationRecord
   has_many :doctors
   
+  def full_address
+    "#{address} #{city}, #{state} #{zip}"
+  end 
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Hospital, type: :model do
   describe 'relationships' do
     it { should have_many :doctors }
   end
+
+  describe 'instance methods' do
+    it "full_address" do 
+      hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+
+      expect(hospital1.full_address).to eq("#{hospital1.address} #{hospital1.city}, #{hospital1.state} #{hospital1.zip}")
+    end 
+  end
 end
