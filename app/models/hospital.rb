5d40a546abef453ad4caff3686ca4103975da88a
Taylor Keller <takeller17@gmail.com>
2020-06-08 09:28:38 -0600

    Sets database up with fleshed out schema and models

---
+++ app/models/hospital.rb
@@ -0,0 +1,8 @@
+class Hospital < ApplicationRecord
+  has_many :doctors
+  validates_presence_of :name,
+                        :address,
+                        :city,
+                        :state,
+                        :zip
+end
---
+++ spec/models/doctor_patient_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe DoctorPatient do
+  describe 'Relationships' do
+    it {should belong_to :doctor}
+    it {should belong_to :patient}
+  end
+
+end 
