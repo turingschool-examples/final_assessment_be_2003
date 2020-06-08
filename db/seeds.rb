25a8247e845dd99177dde92e7cbc8adccf89a535
Melanie Tran <melaniexx02@gmail.com>
2020-06-08 09:20:02 -0600

    Add ReadMe and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+#hospitals
+memorial = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+seaside = Hospital.create(name: "Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001")
+
+#doctors
+grey = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", eduction: "Harvard University", hospital_id: memorial.id)
+karev = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: memorial.id)
+yang = Doctor.create(name: "Cristina Yang", specialty: "Cardiothoracic Surgery", education: "Stanford University", hospital_id: memorial.id)
+shepherd = Doctor.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania", hospital_id: memorial.id)
+sloan = Doctor.create(name: "Mark McSteamy Sloan", specialty: "Plastic Surgeron", education: "University of Pennsylvania", hospital_id: memorial.id)
+
+#Patients
+bryce = Patient.create(name: "Katie Bryce", age: 24)
+duqette = Patient.create(name: "Denny Duquette", age: 39)
+zola = Patient.create(name: "Zola Shepherd", age: 2)
