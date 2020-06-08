e62aaf6888ceadb0a6da6b0136512c960eacc285
Michael Gallup <Gallup93@icloud.com>
2020-06-08 09:39:30 -0600

    Add patient model spec

---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Patient, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :waiting? }
+  end
+
+  describe "relationships" do
+    it {should have_many :doc_patients}
+    it {should have_many(:doctors).through(:doc_patients)}
+  end
+end
