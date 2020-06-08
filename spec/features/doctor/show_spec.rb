cc6336252713003fdec48d4384c9ac89e03f3ffb
Danny Ramos <danramos21@gmail.com>
2020-06-08 13:38:44 -0600

    User Story 5-delete patient spec

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -4,14 +4,12 @@ RSpec.describe "When I visit a doctor's show page" do
   before(:each) do
     @grey_sloan = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: 98101 )
     @seaside = Hospital.create(name: "Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zip: 90001 )
-    @merideth = @grey_sloan.doctors.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")
-    @alex = @grey_sloan.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
     @miranda = @seaside.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
     @mcdreamy = @seaside.doctors.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania")
-    @katie_b = @merideth.patients.create(name: "Katie Bryce", age: 24)
-    @denny_d = @alex.patients.create(name: "Denny Duquette", age: 39)
     @rebecca_p = @mcdreamy.patients.create(name: "Rebecca Pope", age: 32)
     @zola = @mcdreamy.patients.create(name: "Zola Shepherd", age: 2)
+    @rico = @mcdreamy.patients.create(name: "Rico Suave", age: 22)
+    @mimi = @mcdreamy.patients.create(name: "Mimi Mimison", age: 88)
   end
   it "I see all of that doctor's information and patients" do 
 
@@ -23,9 +21,30 @@ RSpec.describe "When I visit a doctor's show page" do
     expect(page).to have_content("Hospital: #{@seaside.name}")
     expect(page).to have_content("Rebecca Pope")
     expect(page).to have_content("Zola Shepherd")
-    expect(page).to_not have_content(@alex.name)
+    expect(page).to have_content("Rico Suave")
+    expect(page).to have_content("Mimi Mimison")
+    expect(page).to_not have_content(@miranda.name)
+  end
+
+  it "Next to each patient's name, I see a button to remove that patient from that doctor's caseload" do 
+
+    visit "/doctors/#{@mcdreamy.id}"
+
+    within(".doctor-#{@zola.id}") do
+      click_button "Delete Patient"
+    end
+
+    expect(page).to_not have_content("Zola Shepherd")
   end
 end
+
+# User Story 5, Remove a Patient from a Doctor
+# As a visitor
+# When I visit a Doctor's show page
+# Next to each patient's name, I see a button to remove that patient from that doctor's caseload
+# When I click that button for one patient
+# I'm brought back to the Doctor's show page
+# And I no longer see that patient's name listed
 
 # User Story 1, Doctors Show Page
 # 'As a visitor
