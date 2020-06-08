3e5c325f060f2e20184ab93195d9562b559c1afd
Mad Hallé <mad.halle@icloud.com>
2020-06-08 14:42:11 -0600

    clean up

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -34,18 +34,40 @@ RSpec.describe Hospital, type: :model do
         age: 32)
     end
 
+    it "#patients_by_age" do
+      expect(@greys.patients_by_age.first).to eq(@denny)
+      expect(@greys.patients_by_age.last).to eq(@katie)
+      expect(@greys.patients_by_age).to eq([@denny,@rebecca,@katie])
+    end
+  end
+  describe "class methods" do
+    before :each do
+      @greys = Hospital.create!(name: "Grey Sloan Memorial Hospital",
+        address: "123 Save Lives Rd",
+        city: "Seattle",
+        state: "WA",
+        zip: 98101)
+
+
+      @meredith = @greys.doctors.create!(name: "Meredith Grey",
+        specialty: "General Surgery",
+        education: "Harvard University")
+
+      @alex = @greys.doctors.create!(name: "Alex Karev",
+        specialty: "Pediatric Surgery",
+        education: "Johns Hopkins University")
+
+      @miranda = @greys.doctors.create!(name: "Miranda Bailey",
+        specialty: "General Surgery",
+        education: "Stanford University")
+
+    end
     it "#number_of_doctors" do
       expect(@greys.number_of_doctors).to eq(3)
     end
 
     it "#doctor_universities" do
       expect(@greys.doctor_universities).to eq(["Harvard University","Johns Hopkins University", "Stanford University"])
-    end
-
-    it "#patients_by_age" do
-      expect(@greys.patients_by_age.first).to eq(@denny)
-      expect(@greys.patients_by_age.last).to eq(@katie)
-      expect(@greys.patients_by_age).to eq([@denny,@rebecca,@katie])
     end
   end
 end
