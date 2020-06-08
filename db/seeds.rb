dca54ae4929a0c1b453856cda8cdedd8855a0fdb
Mad Hallé <mad.halle@icloud.com>
2020-06-08 09:23:49 -0600

    seed file set up, testing begun

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#Doctors:
+
+@meredith = Doctor.create!(name: "Meredith Grey",
+                          specialty: "General Surgery",
+                          education: "Harvard University")
+
+@alex = Doctor.create!(name: "Alex Karev",
+specialty: "Pediatric Surgery",
+education: "Johns Hopkins University")
+
+@miranda = Doctor.create!(name: "Miranda Bailey",
+specialty: "General Surgery",
+education: "Stanford University")
+
+@mcdreamy = Doctor.create!(name: "Derek McDreamy Shepherd",
+specialty: "Attending Surgeon",
+education: "University of Pennsylvania")
+
+
+#Hospitals:
+
+@greys = Hospital.create!(name: "Grey Sloan Memorial Hospital",
+address: "123 Save Lives Rd",
+city: "Seattle",
+state: "WA",
+zip: 98101)
+
+@seaside = Hospital.create!(name: "Seaside Health & Wellness Center",
+address: "123 Private Practice Road",
+city: "Los Angeles",
+state: "CA",
+zip: 90001)
+
+
+#Patients:
+
+@katie = Patient.create!(name: "Katie Bryce",
+age: 24)
+
+@denny = Patient.create!(name: "Denny Duquette",
+age: 39)
+
+@rebecca = Patient.create!(name: "Rebecca Pope",
+age: 32)
+
+@zola = Patient.create!(name: "Zola  Shepherd",
+age: 2)
---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+RSpec.describe "when visiting a doctors show page" do
+  before :each do
+    @greys = Hospital.create!(name: "Grey Sloan Memorial Hospital",
+    address: "123 Save Lives Rd",
+    city: "Seattle",
+    state: "WA",
+    zip: 98101)
+
+
+    @meredith = @greys.doctors.create!(name: "Meredith Grey",
+                              specialty: "General Surgery",
+                              education: "Harvard University")
+
+
+    @katie = @meredith.patients.create!(name: "Katie Bryce",
+                                        age: 24)
+    @denny = @meredith.patients.create!(name: "Denny Duquette",
+                                        age: 39)
+    @rebecca = @meredith.patients.create!(name: "Rebecca Pope",
+                                          age: 32)
+  end
+  it "a visitor will see all doctors info, hospital & patients" do
+    visit "/doctors/#{@meredith.id}"
+
+    expect(page).to have_content(@meredith.name)
+    expect(page).to have_content(@meredith.specialty)
+    expect(page).to have_content(@meredith.education)
+    expect(page).to have_content(@meredith.hospital)
+    expect(page).to have_content(@meredith.patients.all)
+  end
+
+end
+# '```
+# User Story 1, Doctors Show Page
+# 'As a visitor
+# When I visit a doctor's show page
+# I see all of that doctor's information including:
+#  - name
+#  - specialty
+#  - university where they got their doctorate
+# And I see the name of the hospital where this doctor works
+# And I see the names of all of the patients this doctor has
+# ```
