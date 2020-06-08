cf5fe0ee38792e671ac2d4a29edbb9c9cbbf6b81
Melanie Tran <melaniexx02@gmail.com>
2020-06-08 13:05:41 -0600

    Moved order by age method to patient model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,8 +2,4 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
-
-  def self.order_patients_by_age
-
-  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,7 @@ class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
 
-  def self.order_patients_by_age
-    orer(age: :desc)
+  def self.order_by_age
+    order(age: :desc)
   end
 end
