8a9d1b789e39720b9d1c733d9dce68c2f4f17451
Derek Borski <derek.borski@gmail.com>
2020-06-08 09:37:30 -0600

    Create doctors table and hospitals relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608152555) do
+ActiveRecord::Schema.define(version: 20200608153555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.string "education"
+    t.bigint "hospital_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
+  end
 
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 20200608152555) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doctors", "hospitals"
 end
