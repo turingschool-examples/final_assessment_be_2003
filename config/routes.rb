afba04b4dd106ecbc06df4ef8e2ecbbe0559d56e
Michael Gallup <Gallup93@icloud.com>
2020-06-08 12:36:50 -0600

    Add 'new' doctor route and view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "/doctors/:id", to: "doctors#show"
+  post "/doctors/:id", to: "doctors#create"
+  get "/hospitals/:id/new", to: "doctors#new"
 
   get "/hospitals/:id", to: "hospitals#show"
+
   get "/hospitals/:id/patients", to: "patients#index"
-  get "/hospitals/:id/new", to: "doctors#new"
 end
