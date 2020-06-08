de2657a892f6e97a4969d614d7db0a145ab5df57
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-08 13:38:36 -0600

    update patients#destroy action

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,9 @@
 class PatientsController < ApplicationController
   def destroy
-    patient = Patient.find(params[:id])
+    # binding.pry
+    patient = Patient.find(params[:patient_id])
     doctor_patient = DoctorPatient.where(params[:id])
-    doctor_patient.delete
+    doctor_patient.delete_all
     redirect_to "/doctors/#{params[:doctor_id]}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/hospitals/:id", to: "hospitals#show"
   get "/hospitals/:hospital_id/doctors/new", to: "doctors#new"
   post "/hospitals/:hospital_id/doctors", to: "doctors#create"
-  delete "/doctors/:doctor_id/patients/:id", to: "patients#destroy"
+  delete "/doctors/:doctor_id/patients/:patient_id", to: "patients#destroy"
   get "/doctors/:id/edit", to: "doctors#edit"
   patch "/doctors/:id", to: "doctors#update"
   get "/hospitals/:id/patients", to: "hospitals_patients#index"
