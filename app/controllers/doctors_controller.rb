78796ed964486b28268c81e1247cda4a19701509
Jack Gould <johnhilsmangould@gmail.com>
2020-06-08 11:52:59 -0600

    add methods and views

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,4 +4,24 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
   end
 
+  def new
+    @hospital = Hospital.find(params[:id])
+  end
+
+  def create
+    hospital = Hospital.find(params[:hospital_id])
+    hospital.doctors.create!(new_doctor_params)
+    redirect_to "/hospitals/#{hospital.id}"
+  end
+
+  private
+    def new_doctor_params
+      params.permit(
+        :name,
+        :specialty,
+        :education
+      )
+    end
+
+
 end
