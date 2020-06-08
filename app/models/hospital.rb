d3929f63bf35bc05689b72ebc61150ddde827c53
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-08 12:29:37 -0600

    add hospital model

---
+++ app/models/hospital.rb
@@ -0,0 +1,21 @@
+class Hospital < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
+
+  has_many :doctors
+
+  def number_of_doctors
+    doctors.length
+  end
+
+  def unique_schools
+    doctors.select(:education).distinct
+  end
+
+  def doctor_patients
+    Patient.joins(:doctor_patients).joins(:doctors).where('doctors.hospital_id = ?',self.id).order(:age)
+  end
+end
