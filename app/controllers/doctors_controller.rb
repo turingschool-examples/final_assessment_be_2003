b764f29a90d10cea8ce0a64d54353e72844511bf
Alex Pariseau <arpariseau@gmail.com>
2020-06-08 12:08:46 -0600

    Doctor creation

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,20 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def new
+    @hospital = Hospital.find(params[:hospital_id])
+    @doctor = Doctor.new
+  end
+
+  def create
+    hospital = Hospital.find(params[:hospital_id])
+    hospital.doctors.create(doc_params)
+    redirect_to hospital_path(hospital.id)
+  end
+
+  private
+  def doc_params
+    params.require(:doctor).permit(:name, :specialty, :education)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :doctors, only: [:show]
   resources :hospitals, only: [:show] do
     resources :patients, only: [:index]
+    resources :doctors, only: [:new, :create]
   end
 end
