cece594e2d5a1ac345a695f3edd18189424b1c65
Sage Lee <sagemcklee@gmail.com>
2020-06-08 09:18:44 -0600

    move seeds from schema to actual seed file

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,5 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-@meredith = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")
-@alex = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
-@miranda = Doctor.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
-@derek = Doctor.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania")
-
-@sloan = Hospital.create(name: "Grey Sloan Memorial Hospital", address:"123 Save Lives Rd", city: "Seattle", state: "WA", zipcode: "98101")
-@seaside = Hospital.create(name:"Seaside Health & Wellness Center", address: "123 Private Practice Road", city: "Los Angeles", state: "CA", zipcode: "90001")
-
-@katie = Patient.create(name: "Katie Bryce", age: 24)
-@denny = Patient.create(name: "Denny Duquette", age: 39)
-@rebecca = Patient.create(name: "Rebecca Pope", age: 32)
-@zola = Patient.create(name: "Zola  Shepherd", age: 2)
 
 end
