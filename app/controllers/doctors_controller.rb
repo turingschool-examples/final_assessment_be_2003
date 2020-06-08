670b7f0962d09023160c6eec05479fa9587e0c88
Mariana Cid <cidmariana5@gmail.com>
2020-06-08 15:03:41 -0600

    new create method

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,4 +4,20 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.find(params[:id])
   end
 
+  def new
+    @hospital = Hospital.find(params[:id])
+  end
+
+  def create
+    hospital = Hospital.find(params[:id])
+    doctor = hospital.doctors.create(doctor_params)
+    doctor.save
+    redirect_to hospital_path
+  end
+
+  private
+
+  def doctor_params
+    params.permit(:name, :specialty, :education)
+  end
 end
