5871cf26d1cf8c7a1ade6ce21519c08493451b7a
Max Mitrani <mjmitrani168@gmail.com>
2020-06-08 09:40:10 -0600

    add model validation specs

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :specialty
-  validates_presence_of ::education
+  validates_presence_of :education
   has_many :doctor_patients
   has_many :patients, through: :doctor_patients
   belongs_to :hospital
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200608152356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.string "specialty"
+    t.string "education"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hospitals", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
