7d45643f0200fa91d0fc96a59035b2dbcb293c26
Derek Borski <derek.borski@gmail.com>
2020-06-08 11:53:36 -0600

    sort_by_age class method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,7 @@ class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
   
+  def self.sort_by_age(order)
+    order(age: 'desc')
+  end 
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Patient, type: :model do
     it { should have_many :doctor_patients }
     it { should have_many(:doctors).through(:doctor_patients) }
   end
+
+  describe 'class methods' do
+    it "sort_by_age" do
+      patient1 = Patient.create!(name: "Katie Bryce", age: 24)
+      patient2 = Patient.create!(name: "Denny Duquette", age: 39)
+      patient3 = Patient.create!(name: "Rebecca Pope", age: 32)
+
+      patients = Patient.all
+
+      expect(patients.sort_by_age('desc')).to eq([patient2, patient3, patient1])
+    end 
+  end
 end
