f99444749e0fa48bdaeb2bea943e536f697ba907
Jack Gould <johnhilsmangould@gmail.com>
2020-06-08 11:52:34 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get '/hospitals/:id', to: 'hospitals#show'
 
   get '/hospitals/:id/patient_index', to: 'hospitals#index'
+
+  get '/hospitals/:id/new_doctor', to: 'doctors#new'
+  post '/doctors', to: 'doctors#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
