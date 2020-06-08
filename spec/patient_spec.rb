2c966860d0762b1880795cf51e7f0dbfb20c49c1
Eric Larson <ericlarsonbroom@gmail.com>
2020-06-08 09:32:56 -0600

    Created tests for many to many relationship

--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -1,1 +1,8 @@
+require "rails_helper"
 
+RSpec.describe Patient, type: :model do
+  describe "Relationships" do
+    it {should have_many :doctor_patients}
+    it {should have_many(:doctors).through(:doctor_patients)}
+  end
+end
