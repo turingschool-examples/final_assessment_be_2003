ff3f779b2399ab5e1f42682623e91669ecfbfd5b
Melanie Tran <melaniexx02@gmail.com>
2020-06-08 12:05:04 -0600

    Write test for US5

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -18,20 +18,25 @@ RSpec.describe "Doctor's Show Page" do
     expect(page).to have_content("Rebecca Pope")
     expect(page).to have_content("Zola Shepherd")
   end
+
+  it "I can remove patient from doctor's caseload" do
+    memorial = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+    karev = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: memorial.id)
+    pope = Patient.create(name: "Rebecca Pope", age: 32)
+    zola = Patient.create(name: "Zola Shepherd", age: 2)
+    DoctorPatient.create(doctor_id: karev.id, patient_id: pope.id)
+    DoctorPatient.create(doctor_id: karev.id, patient_id: zola.id)
+
+    visit "/doctors/#{karev.id}"
+    expect(page).to have_content("Zola Shepherd")
+    expect(page).to have_content("Rebecca Pope")
+
+    within ".patients-#{pope.id}" do
+      click_link "Remove Patient"
+    end
+
+    expect(current_path).to eq("/doctors/#{karev.id}")
+    expect(page).to have_content("Zola Shepherd")
+    expect(page).to_not have_content("Rebecca Pope")
+  end
 end
-
-
-
-
-
-
-
-# User Story 1, Doctors Show Page
-# 'As a visitor
-# When I visit a doctor's show page
-# I see all of that doctor's information including:
-#  - name
-#  - specialty
-#  - university where they got their doctorate
-# And I see the name of the hospital where this doctor works
-# And I see the names of all of the patients this doctor has
