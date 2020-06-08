d37a1c32973f0bda9f082eee07726cfe3ea023b5
Eric Larson <ericlarsonbroom@gmail.com>
2020-06-08 11:13:42 -0600

    Created show patients test

---
+++ spec/features/hospitals/index_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe "Hospital Patients Index Page" do
+  it "Displays patients names from youngest to oldest" do
+    
+  end
+end
+
+
+# User Story 3, Hospital Patient Index Page
+# As a visitor
+# When I visit the hospitals show page
+# I see a link to view all of that hospitals patients
+# When I click that link
+# I'm taken to a patients index page for that hospital
+# I see the names of all that hospital's patients listed from oldest to youngest
