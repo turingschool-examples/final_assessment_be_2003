f8bcef01a9c7fe0753f6bac9dad116a655dee94c
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-08 12:09:56 -0600

    Add method to hospital model to find all patients

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,7 +1,13 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+  has_many :doctor_patients, through: :doctors
 
   def doctor_universities
     doctors.select(:education).distinct.pluck(:education).to_sentence
   end
+
+  def patients
+    doctor_patients.joins(:patient).select('patients.age').order('patients.age').pluck('patients.name').uniq.join(", ")
+  end
+
 end
