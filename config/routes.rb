3dabb2c06205200ed202524880c89ed7507e5dd6
Jack Cullen <jpcullen20@gmail.com>
2020-06-08 13:25:17 -0600

    Rename hospital patients controller to patients controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors, only: [:show, :edit, :update]
   resources :hospitals, only: [:show] do
-    get '/patients', to: 'hospital_patients#index'
+    resources :patients, only: [:index]
     resources :doctors, only: [:new, :create]
   end
   resources :doctor_patients, only: [:destroy]
