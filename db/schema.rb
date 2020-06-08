8f849317035bbfb70e597e58d86a83aee5babf8a
Jack Cullen <jpcullen20@gmail.com>
2020-06-08 09:53:37 -0600

    Add timestamps to patients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608154501) do
+ActiveRecord::Schema.define(version: 20200608155145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20200608154501) do
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "doctor_patients", "doctors"
