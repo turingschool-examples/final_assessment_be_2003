2363b6446ae5dc79a46d8c8a90682b72b832c235
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-08 15:56:00 -0600

    clean up formatting

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,9 +1,4 @@
 class Hospital < ApplicationRecord
   validates_presence_of :name
   has_many :doctors
-
-  # def order_by_oldest
-  #   x = DoctorPatient.where(doctor_id: self.doctors)
-  #   binding.pry
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "/doctors/:id", to: "doctors#show"
   get "/hospitals/:id", to: "hospitals#show"
   get "/hospitals/:hospital_id/doctors/new", to: "doctors#new"
   post "/hospitals/:hospital_id/doctors", to: "doctors#create"
-  delete "/doctors/:doctor_id/patients/:patient_id", to: "patients#destroy"
+  get "/hospitals/:id/patients", to: "hospitals_patients#index"
+
+  get "/doctors/:id", to: "doctors#show"
   get "/doctors/:id/edit", to: "doctors#edit"
   patch "/doctors/:id", to: "doctors#update"
-  get "/hospitals/:id/patients", to: "hospitals_patients#index"
+  delete "/doctors/:doctor_id/patients/:patient_id", to: "patients#destroy"
 end
