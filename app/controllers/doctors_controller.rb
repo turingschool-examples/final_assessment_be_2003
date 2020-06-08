8eb40085b6ff8c3601cf25603948fded876193f5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-08 14:07:21 -0600

    Add new doctor creation feature and spec

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -6,4 +6,18 @@ class DoctorsController < ApplicationController
   def new
     @hospital = Hospital.find(params[:hospital_id])
   end
+
+  def create
+    hospital = Hospital.find(params[:hospital_id])
+    new_doctor = hospital.doctors.new(doctor_params)
+    if new_doctor.valid?
+      new_doctor.save
+      redirect_to hospital_path(hospital)
+    end
+  end
+
+  private
+  def doctor_params
+    params.permit(:name, :specialty, :education)
+  end
 end
