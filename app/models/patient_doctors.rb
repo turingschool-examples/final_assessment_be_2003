07207b993baef0079c21eb0315105980c2828da9
Jack Gould <johnhilsmangould@gmail.com>
2020-06-08 09:55:17 -0600

    add patient doctors joins table and migration

---
+++ app/models/patient_doctors.rb
@@ -0,0 +1,4 @@
+class PatientDoctors < ApplicationRecord
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608154656) do
+ActiveRecord::Schema.define(version: 20200608155415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20200608154656) do
     t.integer "zip"
   end
 
+  create_table "patient_doctors", force: :cascade do |t|
+    t.bigint "patient_id"
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_patient_doctors_on_doctor_id"
+    t.index ["patient_id"], name: "index_patient_doctors_on_patient_id"
+  end
+
   create_table "patients", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -41,4 +48,6 @@ ActiveRecord::Schema.define(version: 20200608154656) do
   end
 
   add_foreign_key "doctors", "hospitals"
+  add_foreign_key "patient_doctors", "doctors"
+  add_foreign_key "patient_doctors", "patients"
 end
