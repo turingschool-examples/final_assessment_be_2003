c134ade5399ea6b29be050bc93db41aa49551083
Mad Hallé <mad.halle@icloud.com>
2020-06-08 09:38:41 -0600

    patients table created w doctors as foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200608153349) do
+ActiveRecord::Schema.define(version: 20200608153723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,5 +35,15 @@ ActiveRecord::Schema.define(version: 20200608153349) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.bigint "doctor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_patients_on_doctor_id"
+  end
+
   add_foreign_key "doctors", "hospitals"
+  add_foreign_key "patients", "doctors"
 end
