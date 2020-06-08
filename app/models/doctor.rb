065a39308f7de2ec33ffe4a351e7662969660bbf
Mariana Cid <cidmariana5@gmail.com>
2020-06-08 14:27:35 -0600

    new method

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,4 +3,8 @@ class Doctor < ApplicationRecord
   belongs_to :hospital
   has_many :patient_doctors
   has_many :patients, through: :patient_doctors
+
+  def ordered_names
+    patients.order(:age)
+  end
 end
--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -8,4 +8,15 @@ class Hospital < ApplicationRecord
            .distinct
            .pluck('education')
   end
+
+  # def ordered_names
+  #   doctors.each do |doctor|
+  #     doctor.patients.order(:age)
+  #   end
+  #         #.joins(:patients)
+  #         # .select('patients.name')
+  #         # .group('patients.name')
+  #         # .order(doctors.patients.age "ASC")
+  #         # .pluck('patients.name') 
+  # end
 end
