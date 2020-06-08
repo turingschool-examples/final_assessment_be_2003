01fc7931e1c2d34fe753b975bdc55f5d5be1c41f
Taylor Keller <takeller17@gmail.com>
2020-06-08 09:39:23 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,32 +6,72 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Doctors:
-# Name: Meredith Grey
-# Specialty: General Surgery
-# Education: Harvard University
-# Name: Alex Karev
-# Specialty: Pediatric Surgery
-# Education: Johns Hopkins University
-# Name: Miranda Bailey
-# Specialty: General Surgery
-# Education: Stanford University
-# Name: Derek McDreamy Shepherd
-# Specialty: Attending Surgeon
-# Education: University of Pennsylvania
+
 # Hospitals:
-# Grey Sloan Memorial Hospital
-# 123 Save Lives Rd
-# Seattle, WA 98101
-# Seaside Health & Wellness Center
-# 123 Private Practice Road
-# Los Angeles, CA 90001
+
+grey_sloan = Hospital.create(
+  name: "Grey Sloan Memorial Hospital",
+  address: "123 Save Lives Rd",
+  city: "Seattle",
+  state: "WA",
+  zip: 98101
+)
+
+seaside = Hospital.create(
+  name: "Seaside Health & Wellness Center",
+  address: "123 Private Practice Road",
+  city: "Los Angeles",
+  state: "CA",
+  zip: 90001
+)
+
+# Doctors:
+Doctor.create(
+  name: "Meredith Grey",
+  specialty: "General Surgery",
+  university: "Harvard University",
+  hospital_id: grey_sloan.id
+)
+
+Doctor.create(
+  name: "Alex Karev",
+  specialty: "Pediatric Surgery",
+  university: "Johns Hopkins University",
+  hospital_id: grey_sloan.id
+)
+
+Doctor.create(
+  name: "Miranda Bailey",
+  specialty: "General Surgery",
+  university: "Stanford University",
+  hospital_id: seaside.id
+)
+
+Doctor.create(
+  name: "Derek McDreamy Shepherd",
+  specialty: "Attending Surgeon",
+  university: "University of Pennsylvania",
+  hospital_id: seaside.id
+)
+
 # Patients:
-# Name: Katie Bryce
-# Age: 24
-# Name: Denny Duquette
-# Age: 39
-# Name: Rebecca Pope
-# Age: 32
-# Name: Zola  Shepherd
-# Age: 2
+
+Patient.create(
+  name: "Katie Bryce",
+  age: 24
+)
+
+Patient.create(
+  name: "Denny Duquette",
+  age: 39
+)
+
+Patient.create(
+  name: "Rebecca Pope",
+  age: 32
+)
+
+Patient.create(
+  name: "Zola  Shepherd",
+  age: 2
+)
