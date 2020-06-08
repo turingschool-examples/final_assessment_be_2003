2ffe7c89b78b9456fc1ecf19ff795a54d4921f2e
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-06-08 14:44:28 -0600

    update test with patients objects and fixed doctor_patients method and unique_schools to return an array of unique schools

--- spec/model/hospital_spec.rb
+++ spec/model/hospital_spec.rb
@@ -19,6 +19,13 @@ RSpec.describe Hospital, type: :model do
       @doctor1 = @hospital1.doctors.create(name:"Merideth Grey", specialty: "General Surger", education: "harvard University")
       @doctor2 = @hospital1.doctors.create(name:"Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
       @doctor3 = @hospital1.doctors.create(name:"Doctor", specialty: "surgeon", education:"harvard University")
+      @patient1 = Patient.create(name:"Katie Bryce", age: "24")
+      @patient2 = Patient.create(name: "Denny Duquette", age:"39")
+      @patient3 = Patient.create(name: "Rebecca Pope", age:"32")
+      @patient4 = Patient.create(name: "Zola Shepherd", age:"2")
+      @doctorpatient1 = DoctorPatient.create(doctor_id: "#{@doctor1.id}",patient_id: "#{@patient1.id}")
+      @doctorpatient2 = DoctorPatient.create(doctor_id: "#{@doctor1.id}",patient_id: "#{@patient2.id}")
+
     end
 
     it "calculates the number of doctors for the hospital" do
@@ -26,7 +33,11 @@ RSpec.describe Hospital, type: :model do
     end
 
     it "return the unique list of schools associated with the hospital" do
-      expect(@hospital1.unique_schools).to eq()
+      expect(@hospital1.unique_schools).to eq(["harvard University", "Johns Hopkins University"])
+    end
+
+    it "returns the patients associated with that hospitals doctors" do
+      expect(@hospital1.doctor_patients).to eq([@patient1, @patient2])
     end
 
   end
