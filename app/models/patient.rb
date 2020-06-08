2f0baa83a36a6311204466a55b82045e5cd8c53d
Derek Borski <derek.borski@gmail.com>
2020-06-08 14:55:31 -0600

    Complete sort_by_age class method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,9 @@ class Patient < ApplicationRecord
   has_many :doctor_patients
   has_many :doctors, through: :doctor_patients
   
-  def self.sort_by_age(order)
-    order(age: "#{order}")
+  def self.sort_by_age(hospital_id, order)
+    Patient.joins(:doctor_patients => :doctor)
+           .where("doctors.hospital_id = #{hospital_id}")
+           .order("patients.age #{order}")
   end 
 end
