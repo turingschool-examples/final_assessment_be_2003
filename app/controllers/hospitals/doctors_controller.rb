1aea7858b0786b8f51e76fcd94baae5e7b170449
Danny Ramos <danramos21@gmail.com>
2020-06-08 14:22:16 -0600

    Added strong params to Patients Controller

--- app/controllers/hospitals/doctors_controller.rb
+++ app/controllers/hospitals/doctors_controller.rb
@@ -2,11 +2,9 @@ class Hospitals::DoctorsController <ApplicationController
 
   def new
     @hospital = Hospital.find(params[:id])
-    # @doctor = Doctor.new
   end
 
   def create
-    # hospital_id = hospital_id: params[:id]
     @hospital = Hospital.find(params[:id])
     @doctor = @hospital.doctors.create(doctor_params)
     redirect_to "/hospitals/#{@hospital.id}"
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,10 +1,15 @@
 class PatientsController <ApplicationController
 
-  # TODO: strong params for doctor id
   def destroy
     patient = Patient.find(params[:id])
     patient.destroy
-    redirect_to "/doctors/#{params[:doctor_id]}"
+    redirect_to "/doctors/#{doctor_params[:doctor_id]}"
+  end
+
+  private
+
+  def doctor_params
+    params.permit(:doctor_id)
   end
 
 end
