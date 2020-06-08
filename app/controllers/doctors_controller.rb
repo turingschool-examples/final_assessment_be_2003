b2b8ecdf6a399d9338bf1ea9a409acfbb5c5a55e
Michael Gallup <Gallup93@icloud.com>
2020-06-08 11:57:15 -0600

    Add route for "/hospitals/:id/new" to 'new' action in doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,7 @@ class DoctorsController < ApplicationController
   def show
     @doc = Doctor.find(params[:id])
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get "/hospitals/:id", to: "hospitals#show"
   get "/hospitals/:id/patients", to: "patients#index"
+  get "/hospitals/:id/new", to: "doctors#new"
 end
