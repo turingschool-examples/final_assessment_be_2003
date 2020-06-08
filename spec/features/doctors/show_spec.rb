6960a8209fe230d8145b969a2c069ed6be00f8af
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-08 09:49:16 -0600

    complete story 1

---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Doctor Show Page" do
+  it "can see doctor information including hospital name" do
+    grey = Hospital.create(name:"Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+   
+    meredith = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: grey.id)
+    alex = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Stanford University", hospital_id: grey.id)
+
+    visit "/doctors/#{meredith.id}"
+
+    expect(page).to have_content(meredith.name)
+    expect(page).to have_content(meredith.specialty)
+    expect(page).to have_content(meredith.education)
+    expect(page).to have_content(grey.name)
+  end
+
+  it "can see the names of all of the patients this doctor has" do
+    grey = Hospital.create(name:"Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+   
+    meredith = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: grey.id)
+    alex = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Stanford University", hospital_id: grey.id)
+    
+    katie = meredith.patients.create(name: "Katie Bryce", age: 24)
+    denny = meredith.patients.create(name: "Denny Duquette", age: 39)
+
+    visit "/doctors/#{meredith.id}"
+
+    expect(page).to have_content(katie.name)
+    expect(page).to have_content(denny.name)
+    save_and_open_page
+  end
+end
