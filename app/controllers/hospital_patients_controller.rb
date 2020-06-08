b16a11b5655eab1078118b626a50d1fbd1c0f2d1
Derek Borski <derek.borski@gmail.com>
2020-06-08 15:38:30 -0600

    Clean up styling

--- app/controllers/hospital_patients_controller.rb
+++ app/controllers/hospital_patients_controller.rb
@@ -2,7 +2,6 @@ class HospitalPatientsController < ApplicationController
 
   def index
     @hospital = Hospital.find(params[:hospital_id])
-
     @hospital_patients = Patient.sort_by_age(@hospital.id, 'desc')
   end 
 end
