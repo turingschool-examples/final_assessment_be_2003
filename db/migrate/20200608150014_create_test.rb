23a01e638437127674c149a7f97ea3ff9219664d
Ross Perry <perryr16@gmail.com>
2020-06-08 14:49:21 -0600

    remove test tables

--- db/migrate/20200608150014_create_test.rb
+++
@@ -1,7 +0,0 @@
-class CreateTest < ActiveRecord::Migration[5.1]
-  def change
-    create_table :tests do |t|
-      t.string :name
-    end
-  end
-end
--- db/migrate/20200608150148_drop_table_tests.rb
+++
@@ -1,5 +0,0 @@
-class DropTableTests < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :tests
-  end
-end
