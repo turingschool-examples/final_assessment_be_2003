f00148aa2d8f9851327cd36e849a1a18ccac8a76
Melanie Tran <melaniexx02@gmail.com>
2020-06-08 10:18:49 -0600

    Write test for unique education

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -22,5 +22,17 @@ RSpec.describe Hospital do
       expect(memorial.total_number_of_doctors).to_not eq(7)
       expect(seaside.total_number_of_doctors).to eq(1)
     end
+
+    it ".unique_education" do
+      memorial = Hospital.create(name: "Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101")
+      grey = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University", hospital_id: memorial.id)
+      bailey = Doctor.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", hospital_id: memorial.id)
+      karev = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: memorial.id)
+      yang = Doctor.create(name: "Cristina Yang", specialty: "Cardiothoracic Surgery", education: "Stanford University", hospital_id: memorial.id)
+      shepherd = Doctor.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania", hospital_id: memorial.id)
+      sloan = Doctor.create(name: "Mark McSteamy Sloan", specialty: "Plastic Surgeron", education: "University of Pennsylvania", hospital_id: memorial.id)
+
+      expect(memorial.unique_education).to eq(["Harvard University", "Johns Hopkins University", "Stanford University", "University of Pennsylvania"])
+    end
   end
 end
