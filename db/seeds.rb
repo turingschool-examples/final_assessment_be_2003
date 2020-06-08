e24495bda7482a0ca7253f1e18c9a347cdfae72a
Ross Perry <perryr16@gmail.com>
2020-06-08 09:56:30 -0600

    US1 built and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Doctors:
-# Name: Meredith Grey
-# Specialty: General Surgery
-# Education: Harvard University
-# Name: Alex Karev
-# Specialty: Pediatric Surgery
-# Education: Johns Hopkins University
-# Name: Miranda Bailey
-# Specialty: General Surgery
-# Education: Stanford University
-# Name: Derek McDreamy Shepherd
-# Specialty: Attending Surgeon
-# Education: University of Pennsylvania
+
 # Hospitals:
-# Grey Sloan Memorial Hospital
-# 123 Save Lives Rd
-# Seattle, WA 98101
-# Seaside Health & Wellness Center
-# 123 Private Practice Road
-# Los Angeles, CA 90001
+@hospital1 = Hospital.create(name:"Grey Sloan Memorial Hospital", address:"123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101" )
+@hospital2 = Hospital.create(name:"Seaside Health & Wellness Center", address:"123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001" )
+
+# Doctors:
+@doctor1 = @hospital1.doctors.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")
+@doctor2 = @hospital1.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
+@doctor3 = @hospital1.doctors.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University")
+@doctor4 = @hospital2.doctors.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgery", education: "University of Pennyslvania")
+
 # Patients:
-# Name: Katie Bryce
-# Age: 24
-# Name: Denny Duquette
-# Age: 39
-# Name: Rebecca Pope
-# Age: 32
-# Name: Zola  Shepherd
-# Age: 2
+@patient1 = Patient.create(name: "Katie Bryce", age: 24)
+@patient2 = Patient.create(name: "Denny Duquette", age: 39)
+@patient3 = Patient.create(name: "Rebecca Pope", age: 32)
+@patient4 = Patient.create(name: "Zola  Shepherd", age: 2)
---
+++ spec/features/doctors/show_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe "Dr Show Page" do
+  before :each do
+    @hospital1 = Hospital.create(name:"Grey Sloan Memorial Hospital", address:"123 Save Lives Rd", city: "Seattle", state: "WA", zip: "98101" )
+    @hospital2 = Hospital.create(name:"Seaside Health & Wellness Center", address:"123 Private Practice Road", city: "Los Angeles", state: "CA", zip: "90001" )
+
+    @doctor1 = @hospital1.doctors.create(name: "Meredith Grey", specialty: "General Surgery", education: "Harvard University")
+    @doctor2 = @hospital1.doctors.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University")
+
+    @patient1 = Patient.create(name: "Katie Bryce", age: 24)
+    @patient2 = Patient.create(name: "Denny Duquette", age: 39)
+    @patient3 = Patient.create(name: "Rebecca Pope", age: 32)
+    @patient4 = Patient.create(name: "Zola  Shepherd", age: 2)
+
+    @doctor1.patients << @patient1
+    @doctor1.patients << @patient2
+    @doctor1.patients << @patient3
+    @doctor2.patients << @patient4
+
+  end
+  it "US1 Doctor show page has info about doctors, patients and its hospital" do
+    visit doctor_path(@doctor1)
+
+    within("#doctor-info")do
+      expect(page).to have_content("Name: #{@doctor1.name}")
+      expect(page).to have_content("Specialty: #{@doctor1.specialty}")
+      expect(page).to have_content("Education: #{@doctor1.education}")
+      expect(page).to_not have_content("Name: #{@doctor2.name}")
+      expect(page).to_not have_content("Specialty: #{@doctor2.specialty}")
+      expect(page).to_not have_content("Education: #{@doctor2.education}")
+    end
+
+    within("#doctor-hospital")do
+      expect(page).to have_content("Works at: #{@hospital1.name}")
+      expect(page).to_not have_content(@hospital2.name)
+    end
+
+    expect(page).to have_content("Patients:")
+    within("#patient-#{@patient1.id}")do
+      expect(page).to have_content(@patient1.name)
+      expect(page).to_not have_content(@patient2.name)
+    end
+    within("#patient-#{@patient2.id}")do
+      expect(page).to have_content(@patient2.name)
+      expect(page).to_not have_content(@patient3.name)
+    end
+    within("#patient-#{@patient3.id}")do
+      expect(page).to have_content(@patient3.name)
+    end
+    expect(page).to_not have_content(@patient4.name)
+
+ 
+    
+  end
+  
+end
+
+# User Story 1, Doctors Show Page
+# 'As a visitor
+# When I visit a doctor's show page
+# I see all of that doctor's information including:
+#  - name
+#  - specialty
+#  - university where they got their doctorate
+# And I see the name of the hospital where this doctor works
+# And I see the names of all of the patients this doctor has
