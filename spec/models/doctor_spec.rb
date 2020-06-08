488a7d9f0bc7ee085a1a92cf30cab82bfc419311
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-06-08 09:46:27 -0600

    Add doctor model test

---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Doctor, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :specialty}
+    it {should validate_presence_of :education}
+  end
+
+  describe "relationships" do
+    it {should belong_to :hospital}
+  end
+end
