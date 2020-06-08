7064d48d76027759c81111309c38bbd71b02bc71
Antonio Jackson <antoniojackson@gmail.com>
2020-06-08 14:43:56 -0600

    Removing commented out doctors#update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   resources :doctors, only: [:show, :edit, :update]
   delete '/doctors/:doctor_id/patients/:patient_id', to: 'doctor_patients#destroy'
-  # patch '/doctors/:doctor_id', to: 'doctors#update'
 
   get '/hospitals/:id', to: 'hospitals#show'
   get '/hospitals/:hospital_id/patients', to: 'hospital_patients#index'
