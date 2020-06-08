b9db01ab46207e931eec4d86036ac3415e88f9ae
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-08 13:16:46 -0600

    Create new method in doctors controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def new
+    @hospital = Hospital.find_by(id: params[:hospital_id])
+  end
+
+  def create
+  end
 end
