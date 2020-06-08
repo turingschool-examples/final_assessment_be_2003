0e7da2aed52d17c9903066ace03313274d0fce59
Michael Gallup <Gallup93@icloud.com>
2020-06-08 13:58:59 -0600

    Add doctor edit spec

---
+++ spec/features/doctors/edit_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'doctor edit page', type: :feature do
+  before(:each) do
+    @greys = Hospital.create(name:"Grey Sloan Memorial Hospital", address: "123 Save Lives Rd", city: "Seattle", state: "Washington", zip: "98101")
+    @seaside = Hospital.create(name:"Seaside Health & Wellness Center", address: "123 Private Practice Rd", city: "LA", state: "California", zip: "90210")
+
+    @meredith = @greys.doctors.create(name: "Meredith Grey", specialty: "general surgery", school: "Turing")
+  end
+
+  it "reassigns a doctor to new hospital via provided hospital id" do
+    visit "/doctors/#{@meredith.id}"
+
+    expect(page).to have_content("works at: Grey Sloan Memorial Hospital")
+    click_link "reassign"
+
+    expect(current_path).to eq("/doctors/#{@meredith.id}/edit")
+    fill_in :hospital_id, with: @seaside.id
+
+    click_button "Reassign Doctor"
+
+    expect(current_path).to eq("/doctors/#{@meredith.id}")
+    expect(page).to have_content("works at: Seaside Health & Wellness Center")
+  end
+end
