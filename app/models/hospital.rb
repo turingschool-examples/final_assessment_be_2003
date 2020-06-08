44ca869da7164efff3a268a0f0f99ed6581a03ad
Daniel Selph <daniel.p.selph@gmail.com>
2020-06-08 11:16:14 -0600

    Create hospital show page, controller and view

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,13 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doc_count
+    doctors.count
+  end
+
+  def doc_universities
+    doctors.select(:education).distinct.pluck(:education)
+  end
+  
+  
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe Hospital, type: :model do
   describe "relationships" do
     it { should have_many :doctors}
   end
+
+  describe "methods" do
+    before(:each) do
+      @hospital1 = Hospital.create(name: "Banner Desert", address: "123 Medical Dr", city: "Mesa", state: "AZ", zip: "80202")
+      
+      @doc1 = @hospital1.doctors.create(name: "John Matrix", specialty: "Cardiology", education: "Arizona State University")
+      @doc2 = @hospital1.doctors.create(name: "Doug Quiad", specialty: "Neurology", education: "Metro State University")
+      @doc3 = @hospital1.doctors.create(name: "Harry Tasker", specialty: "Pulmonology", education: "Arizona State University")
+    end
+
+    it "#doc_count" do
+      expect(@hospital1.doc_count).to eq(3) 
+    end
+    
+    
+  end
+  
 end
