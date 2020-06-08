4c0033be7eaa45453ec897f89dfdc11d146c6fbd
Melanie Tran <melaniexx02@gmail.com>
2020-06-08 10:12:11 -0600

    Create test for total_number_of_doctors

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@ grey = Doctor.create(name: "Meredith Grey", specialty: "General Surgery", educti
 karev = Doctor.create(name: "Alex Karev", specialty: "Pediatric Surgery", education: "Johns Hopkins University", hospital_id: memorial.id)
 yang = Doctor.create(name: "Cristina Yang", specialty: "Cardiothoracic Surgery", education: "Stanford University", hospital_id: memorial.id)
 shepherd = Doctor.create(name: "Derek McDreamy Shepherd", specialty: "Attending Surgeon", education: "University of Pennsylvania", hospital_id: memorial.id)
-sloan = Doctor.create(name: "Mark McSteamy Sloan", specialty: "Plastic Surgeron", education: "University of Pennsylvania", hospital_id: memorial.id)
+sloan = Doctor.create(name: "Mark McSteamy Sloan", specialty: "Plastic Surgery", education: "University of Pennsylvania", hospital_id: memorial.id)
 bailey = Doctor.create(name: "Miranda Bailey", specialty: "General Surgery", education: "Stanford University", hospital_id: memorial.id)
+avery = Doctor.create(name: "Jackson Avery", specialty: "Plastic Surgery", education: "Boston University", hospital_id: seaside.id)
 
 #Patients
 bryce = Patient.create(name: "Katie Bryce", age: 24)
