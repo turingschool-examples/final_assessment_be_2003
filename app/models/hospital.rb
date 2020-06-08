f4f4a1669086f739b0f8e810fb5e0ce307f55165
Max Mitrani <mjmitrani168@gmail.com>
2020-06-08 09:32:44 -0600

    add many to many relation in model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,2 +1,8 @@
 class Hospital < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
+  has_many :doctors
 end
