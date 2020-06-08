3a0b0ce0ea6a27dd4dbd28f7785d7dd9c76ebaad
stellakunzang <stella.k.bonnie@gmail.com>
2020-06-08 10:50:04 -0600

    Add method to hospital model to generate list of unique dr. universities

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,7 @@
-class Hospital < ApplicationRecord 
+class Hospital < ApplicationRecord
   has_many :doctors
+
+  def doctor_universities
+    doctors.select(:education).distinct.pluck(:education).to_sentence
+  end
 end
